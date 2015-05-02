8261a3c8f5c5408e2dfc1380a84c8ea4b9da171f
luluphotog <ldgoncalves@fullsail.edu>
2015-05-02 18:56:14 -0400

    person.js pseudocode

--- Wk4/goal7_assign_objects/js/person.js
+++ Wk4/goal7_assign_objects/js/person.js
@@ -6,4 +6,8 @@
  * Activity: Analyze Objects.person (Pseudocode)
  */
 
+//Make the the Person object globally for main.js: window.Person=Person
 
+//Under Person object, declare jobs and actions and give them each an array with 4 or more tasks:Person.jobs=["teacher", "farmer","student","pilot"]; Person.actions = ["sleeping", "eating", "working"];
+
+//Create four properties for the Person Constructor: name, action, job, and row
