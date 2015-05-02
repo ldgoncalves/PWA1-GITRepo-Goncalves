7aa6d7159ac85df792aa3dc9a7b1ddd8583fe223
luluphotog <ldgoncalves@fullsail.edu>
2015-05-02 19:25:05 -0400

    edits to main.js

--- Wk4/goal7_assign_objects/js/main.js
+++ Wk4/goal7_assign_objects/js/main.js
@@ -12,7 +12,7 @@
 
 //Create an object using a for loop using math.random that delivers 3 instances and adds these references into the people array.
 
-//Make the in the math.random formula sends to the constructor of person (new Person) and show the HTML row number it will be displayed  with the random chosen name from the names array
+//Make in the math.random formula sends to the constructor of person (new Person) and show the HTML row number it will be displayed  with the chosen name from the names array
 
 // Create a function that returns person's name, job and webpage. Name the function "populateHTML"
 
@@ -21,6 +21,6 @@
 // setInterval(runUpdate, 1000/30); -  this interval with control the amount of time the random name will be selected to every 30 min
 
 
-// Once the people array have all the Person Instances, create a loop to run this function for each person: function runUpdate(){people.forEach(funcation(element){element.update();});
+// Once the people array have all the Person Instances, create a loop to run this function for each person: function runUpdate(){people.forEach(function(element){element.update();});
 
 
