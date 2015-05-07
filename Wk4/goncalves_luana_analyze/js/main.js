795b00df5349d0a3f3ba4036a7b43263684cc534
luluphotog <ldgoncalves@fullsail.edu>
2015-05-07 15:35:33 -0400

    final

---
+++ Wk4/goncalves_luana_analyze/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ * Name: Luana Goncalves
+ * Activity: Analyze Objects.person (Pseudocode)
+ */
+
+//Pseudocode for main.js
+
+//Create an array var name = [] and add 5 people names to be randomly selected.
+
+//Create an object using a for loop using math.random that delivers 3 instances and adds these references into the people array.
+
+//Make in the math.random formula sends to the constructor of person (new Person) and show the HTML row number it will be displayed  with the chosen name from the names array
+
+// Create a function that returns person's name, job and webpage. Name the function "populateHTML"
+
+//set up a conditional in the function that stop duplicate names from populating
+
+// setInterval(runUpdate, 1000/30); -  this interval with control the amount of time the random name will be selected to every 30 min
+
+
+// Once the people array have all the Person Instances, create a loop to run this function for each person: function runUpdate(){people.forEach(function(element){element.update();});
+
+
---
+++ Wk4/goncalves_luana_analyze/js/person.js
@@ -0,0 +1,13 @@
+/**
+ * Created by the JavaScript Development Team
+ * Class: PWA
+ * Goal: Goal7
+ * Name: Luana Goncalves
+ * Activity: Analyze Objects.person (Pseudocode)
+ */
+
+//Make the the Person object globally for main.js: window.Person=Person
+
+//Under Person object, declare jobs and actions and give them each an array with 4 or more tasks:Person.jobs=["teacher", "farmer","student","pilot"]; Person.actions = ["sleeping", "eating", "working"];
+
+//Create four properties for the Person Constructor: name, action, job, and row
